54aef718fee86b08a849c4115d38fccea3ee1a0f
PrathamD07 <132071631+PrathamD07@users.noreply.github.com>
2023-04-28 22:19:23 +0530

    Delete Saksoft directory

--- Saksoft/src/main/java/com/springboot/web/Repository/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.web.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springboot.web.Entity.Person;
-
-public interface PersonRepository extends JpaRepository<Person, Long> {
-
-}
